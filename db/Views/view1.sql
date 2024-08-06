9e4bd6f73a10ef144a58f39241921e231d15a2ba
Dima Bezzubenkov <dima@devscribed.com>
2024-08-05 21:06:52 -0700

    adadadsa

---
+++ db/Views/view1.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW `view1`
+AS
+	SELECT
+	  `o`.`OrderID` AS `OrderID`,
+	  `o`.`CustomerID` AS `CustomerID`,
+	  `o`.`OrderDate` AS `OrderDate`,
+	  `o`.`TotalAmount` AS `TotalAmount`
+	FROM `orders` `o`;
